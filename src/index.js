17ec17cf343305508b4f7913c1b434bfbc6d7c6c
sirikub <kubyshkinsergei21@gmail.com>
2023-10-18 21:52:04 +0300

    add function filename in webpack.config()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import './scss/index.scss'
+
 console.log('Working')
+
+async function start(){
+    await Promise.resolve()
+}
+
+start()
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,17 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin") //выносим css из js файла
 
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: 'bundle.[hash].js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist')
     },
     resolve:{
@@ -22,17 +27,40 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
+            minify: {
+              removeComments: isProd,
+              collapseWhitespace: isProd
+            }
         }),
-        new CopyPlugin([          
-            { 
-                from: path.resolve(__dirname, 'src/favicon.ico'), 
-                to: path.resolve(__dirname, 'dist') 
-            },     
-        ]),
+        // new CopyPlugin([          
+        //     { 
+        //         from: path.resolve(__dirname, 'src/favicon.ico'), 
+        //         to: path.resolve(__dirname, 'dist') 
+        //     }
+        // ]),
         new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css'
+            filename: filename('css')
         })
-    ]
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [       
+                MiniCssExtractPlugin.loader,   
+              "css-loader",              
+              "sass-loader",
+            ],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: "babel-loader"
+            }
+          },
+        ],
+      },
 }
 
