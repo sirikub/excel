a8d9d13457887ac3d954a688a4783bf97c87efc2
sirikub <kubyshkinsergei21@gmail.com>
2023-10-26 21:16:30 +0300

    new1

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,12 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use: jsLoaders()
+        loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        
+
+      }
       },
     ],
   },
